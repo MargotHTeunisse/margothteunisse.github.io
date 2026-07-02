448c40840d0432a9709c61b0fe0bf78b1fbf1aa8
Margot Teunisse <margotteunisse@hotmail.com>
2026-07-02 18:49:42 +0200

    Create Chirpy theme site; add _site to .gitignore

---
+++ docs/assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1783021380',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
