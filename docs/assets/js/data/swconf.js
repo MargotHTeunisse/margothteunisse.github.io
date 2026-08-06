2f8908f0c3fa42319ab0e031bf0d1c02cf6d119d
Margot Teunisse <margotteunisse@hotmail.com>
2026-08-06 13:04:52 +0200

    Fix minor issues in web API post

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1785943712',resources: [
+    cacheName: 'chirpy-1786014209',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
